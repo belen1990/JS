4b977465f32e33a23aa70ec5233e590b396535f1
belen1990 <belen_lau@hotmail.com>
2018-10-30 17:58:16 +0100

    30/10 tarde

---
+++ AJAX/ajax.js
@@ -0,0 +1,64 @@
+
+function generarHtml(paramElemid, paramAmigos) {
+    $(paramElemid).html(function () {
+        let htmlAmigos = '';
+        paramAmigos.forEach(unAmigo => {
+            htmlAmigos += `<li>
+           <div>${unAmigo.name}</div>
+           <div>${unAmigo.email}</div>
+           <div><button class="saludaBtn" data-value="${unAmigo.email}">
+           Saludar</button></div> </li>`;
+        });
+
+        return htmlAmigos;
+    });
+}
+
+$('#cargaramigosfb').click(function () {
+    $.ajax({
+        url: "http://www.mocky.io/v2/5bd84b1e3100002414474d4b",
+    }).done(function (amigosfromserve) {
+        console.log(amigosfromserve);
+        generarHtml('#amigosfb', amigosfromserve);
+
+        //saludar los botones //asociar eventos
+        $('#amigosfb.saludaBtn').click(function () {
+            alert('hola' + $(this).attr('data-value') + '!!!!!!');
+
+        })
+    });
+
+});
+
+
+
+$('#cargaramigossp').click(function () {
+    $.ajax({
+        url: "http://www.mocky.io/v2/5bd84b1e3100002414474d4b",
+    }).done(function (amigosfromserve) {
+        console.log(amigosfromserve);
+        generarHtml('#amigossp', amigosfromserve);
+
+        //saludar los botones //asociar eventos
+        $('amigossp.saludaBtn').click(function () {
+            alert('hola' + $(this).attr('data-value') + '!!!!!!');
+        //asociar eventos
+        asociaClick('amigosfb',amigosfromserve);
+
+        })
+    });
+
+});
+
+
+$('#cargaramigosfb').click(function () {
+    
+    $ajax({
+        url: "http://www.mocky.io/v2/5bd84b1e3100002414474d4b",
+    }).done(function(amigosfromserve) {
+        console.log(amigosfromserve)
+        generarHtml('amigosfb', amigosfromserve);
+        asociaClick('#amigosgb');
+        
+    })
+})
